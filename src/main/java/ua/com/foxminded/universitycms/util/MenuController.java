9402fb68a2dc012a9d805a5e36b5bd34a2362b59
Spartak <spartaklysman@gmail.com>
2024-03-29 11:56:17 +0100

    c

---
+++ src/main/java/ua/com/foxminded/universitycms/util/MenuController.java
@@ -0,0 +1,13 @@
+package ua.com.foxminded.universitycms.util;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MenuController {
+
+	@GetMapping("/menu")
+	public String showMenu() {
+		return "menu";
+	}
+}
