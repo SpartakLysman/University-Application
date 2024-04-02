687d860bbb4638f5f4cb3b3305b43bf9db0c3966
Spartak <spartaklysman@gmail.com>
2024-04-02 09:00:09 +0200

    Spring course. Security. error bag

--- src/main/java/ua/com/foxminded/universitycms/UniversityCmsApplication.java
+++ src/main/java/ua/com/foxminded/universitycms/UniversityCmsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UniversityCmsApplication {
 
 	public static void main(String[] args) {
+		// System.out.println(new BCryptPasswordEncoder().encode("admin"));
 		SpringApplication.run(UniversityCmsApplication.class, args);
 	}
 }
--- src/main/java/ua/com/foxminded/universitycms/config/SecurityConfig.java
+++ src/main/java/ua/com/foxminded/universitycms/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package ua.com.foxminded.universitycms.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,37 +7,25 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import ua.com.foxminded.universitycms.security.CustomUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(requests -> requests
-                        .requestMatchers("/css/**", "/?continue", "/webjars/**", "/assets/**")
-                        .permitAll()
-                        .requestMatchers("menu")
-                        .hasAnyRole("ADMIN", "TEACHER", "STUDENT")
-                        .anyRequest()
-                        .authenticated())
-                .formLogin(form -> form
-                        .loginPage("/login")
-                        .loginProcessingUrl("/login")
-                        .defaultSuccessUrl("/menu")
-                        .permitAll())
-                .logout(logout -> logout
-                        .logoutSuccessUrl("/logout")
-                        .permitAll()
-                );
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(requests -> requests
+				.requestMatchers("static/css/**", "/?continue", "/webjars/**", "/assets/**").permitAll()
+				.requestMatchers("menu").hasAnyRole("ADMIN", "TEACHER", "STUDENT").anyRequest().authenticated())
+				.formLogin(form -> form.loginPage("/login").loginProcessingUrl("/login").defaultSuccessUrl("/menu")
+						.permitAll())
+				.logout(logout -> logout.logoutSuccessUrl("/logout").permitAll());
 
-        return http.build();
-    }
+		return http.build();
+	}
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
